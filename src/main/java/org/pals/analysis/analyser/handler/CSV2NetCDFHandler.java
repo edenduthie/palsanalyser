42043cc1eda4b3066986e580c5113555747cd79a
Eden Duthie <eduthie@gmail.com>
2013-04-24 16:16:01 +1000

    File copy method is fixed and tested

--- src/main/java/org/pals/analysis/analyser/handler/CSV2NetCDFHandler.java
+++ src/main/java/org/pals/analysis/analyser/handler/CSV2NetCDFHandler.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Logger;
@@ -15,6 +20,22 @@ import org.pals.analysis.request.AnalysisException;
 import org.pals.analysis.request.AnalysisReply;
 import org.pals.analysis.request.AnalysisRequest;
 
+/**
+ * Handler retrieves the remote file and parameters and pass them to the
+ * analysis method. It also pass back the results as a reply. Note: R can
+ * read/delete files using URLs but this layer handles copying/deleting files.
+ * This is to leave R scripts free from such remote client/server needs. Such
+ * are not essential to analysis.
+ * 
+ * It implements only file://localhost/ URL (or without host). If client and
+ * server are on different hosts, it may be better to use URLs other than
+ * file://. Java could open URLConnection and read bytes from the source, but it
+ * may require code to handle security. (URLConnection works with http:// but we
+ * are not using it.)
+ * 
+ * @author Yoichi
+ * 
+ */
 public class CSV2NetCDFHandler implements RequestHandler
 {
 	private final static Logger LOGGER = Logger
@@ -43,10 +64,6 @@ public class CSV2NetCDFHandler implements RequestHandler
 		this.outputDataDirPath = outputDataDirPath;
 	}
 
-	/**
-	 * The method retrieves the remote file and parameters and pass them to the
-	 * analysis method
-	 */
 	public AnalysisReply handleRequest(AnalysisRequest request)
 			throws AnalysisException
 	{
@@ -67,7 +84,7 @@ public class CSV2NetCDFHandler implements RequestHandler
 		String towerHeight = (String) analysisArguments.get(TOWER_HEIGHT);
 
 		URL remoteFileURL;
-		File localCSVFile = null;
+		URL localCSVFileURL = null;
 		Map<String, Object> outputFileURLs = null;
 		try
 		{
@@ -76,13 +93,16 @@ public class CSV2NetCDFHandler implements RequestHandler
 			/**
 			 * Currently, it handles only file:// URLs
 			 */
-			if (FILE_PROTOCOL.equals(protocol)) localCSVFile = getFileViaFileProtocol(
-					requestIdStr, this.inputDataDirPath, remoteFileURL);
+			if (FILE_PROTOCOL.equals(protocol))
+			{
+				localCSVFileURL = getFileViaFileProtocol(requestIdStr,
+						this.inputDataDirPath, remoteFileURL);
+			}
 
 			else
 				throw new AnalysisException("unknown protocol: " + protocol);
 
-			outputFileURLs = convertCSV2NetCDFWithR(localCSVFile,
+			outputFileURLs = convertCSV2NetCDFWithR(localCSVFileURL,
 					this.outputDataDirPath, userName, dataSetName,
 					dataSetVersionName, longitude, latitude, elevation,
 					towerHeight);
@@ -119,44 +139,58 @@ public class CSV2NetCDFHandler implements RequestHandler
 	 * @param towerHeight2
 	 * @return
 	 */
-	private Map<String, Object> convertCSV2NetCDFWithR(File localCSVFile,
+	private Map<String, Object> convertCSV2NetCDFWithR(URL localCSVFileURL,
 			String userName, String dataSetName, String dataSetVersionName,
 			String longitude2, String latitude2, String elevation2,
 			String towerHeight, String towerHeight2)
 	{
-		// TODO Auto-generated method stub
+		// XXX WORK_IN_PROGRESS Auto-generated method stub
 		return null;
 	}
 
 	/**
-	 * This copies the remote file to a local file and deletes the remote file.
-	 * TODO: If both files are on the local fs, the remote just needs to be
-	 * moved/renamed.
+	 * This copies the client's file to a server file and deletes the server
+	 * file. It ignores the "host" part of the URL.
+	 * 
+	 * TODO: When both files are on the local fs, the remote file just needs to
+	 * be moved/renamed.
 	 * 
 	 * @param requestId
 	 * @param inputDataDirPath
 	 * @param remoteFileURL
 	 * @return
 	 * @throws IOException
+	 * @see {@link http
+	 *      ://examples.javacodegeeks.com/core-java/io/file/4-ways-to-
+	 *      copy-file-in-java/}
 	 */
-	private File getFileViaFileProtocol(String requestId,
+	private URL getFileViaFileProtocol(String requestId,
 			String inputDataDirPath, URL remoteFileURL) throws IOException
 	{
-		String remoteFilePathStr = remoteFileURL.getPath();
+		File inputFileDir = new File(inputDataDirPath);
+		if (!inputFileDir.exists()) inputFileDir.mkdirs();
 		String newFilePathStr = inputDataDirPath + File.separator + requestId
 				+ ".csv";
-		File remoteFile = new File(remoteFilePathStr);
 		File newFile = new File(newFilePathStr);
 
+		String remoteFilePath = remoteFileURL.getPath();
+		File remoteFile = new File(remoteFilePath);
+
+		FileInputStream fileInputStream = null;
+		FileOutputStream fileOutputStream = null;
 		FileChannel inputChannel = null;
 		FileChannel outputChannel = null;
 		boolean isDeleted = false;
 		try
 		{
-			inputChannel = new FileInputStream(remoteFile).getChannel();
-			outputChannel = new FileOutputStream(newFile).getChannel();
+			fileInputStream = new FileInputStream(remoteFile);
+			inputChannel = fileInputStream.getChannel();
+			fileOutputStream = new FileOutputStream(newFile);
+			outputChannel = fileOutputStream.getChannel();
 
-			outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
+			long position = 0;
+			outputChannel.transferFrom(inputChannel, position,
+					inputChannel.size());
 
 			isDeleted = remoteFile.delete();
 			if (!isDeleted) LOGGER.warning("cound not delete: "
@@ -165,9 +199,13 @@ public class CSV2NetCDFHandler implements RequestHandler
 		finally
 		{
 			inputChannel.close();
+			if (fileInputStream != null) fileInputStream.close();
 			outputChannel.close();
+			if (fileOutputStream != null) fileOutputStream.close();
 		}
 
-		return newFile;
+		URL newFileURL = new URL("file", "", newFile.getPath());
+
+		return newFileURL;
 	}
 }
