7c54956b7be8d65e5f16636c413ec7a9af98b7ac
Eden Duthie <eduthie@gmail.com>
2013-07-01 15:05:57 +1000

    Trivial thread runner class New

--- src/main/java/org/pals/analysis/run/AnalysisServerRunner.java
+++ src/main/java/org/pals/analysis/run/AnalysisServerRunner.java
@@ -1,4 +1,4 @@
-package org.pals.analysis.server;
+package org.pals.analysis.run;
 
 import javax.servlet.ServletException;
 
@@ -10,34 +10,45 @@ import com.opensymphony.xwork2.util.logging.Logger;
 import com.opensymphony.xwork2.util.logging.LoggerFactory;
 
 /**
- * A trivial class to run PALS analysis server as a stand-alone Spring bean
+ * A trivial class to simulate a Servlet container without HTTP API.
+ * 
+ * TestAnalisysClient will show how to use this class.
+ * 
+ * Since this is just a thread, you cannot send a stop signal, e.g., via an HTTP port. If the
+ * main holds the thread instance, you can send "interrupt" to the thread to stop the main loop.
  * 
  * @author Yoichi
  * 
  */
-public class AnalysisServerRunner
+public class AnalysisServerRunner implements Runnable
 {
-    final static Logger logger = LoggerFactory.getLogger(AnalysisServerRunner.class);
-    
-    /**
-     * Main method.
-     */
-    public static void main(String[] args) {
-        logger.info("Initializing Spring context.");
-        
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/applicationContext.xml");
-        
-        logger.info("Spring context initialized.");
+	final static Logger logger = LoggerFactory
+			.getLogger(AnalysisServerRunner.class);
 
-        AnalysisServlet servlet = (AnalysisServlet) applicationContext.getBean("analysisServer");
-        
-        servlet.setRunningAsMain(true);
-        servlet.setRunningAsServlet(false);
+	private static AnalysisServlet servlet = null;
+
+	/**
+	 * This must be called via Thread's start();
+	 */
+	public void run()
+	{
+		logger.info("Initializing Spring context.");
+
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"/applicationContext.xml");
+
+		logger.info("Spring context initialized.");
+
+		servlet = (AnalysisServlet) applicationContext
+				.getBean("analysisServer");
+
+		servlet.setRunningAsMain(true);
+		servlet.setRunningAsServlet(false);
 		try
 		{
 			servlet.init();
 
-			// It must keep running if it is not a Servlet
+			// It must keep running to hold the instance of AlanysisServlet
 			while (servlet.isRunningAsMain())
 			{
 				try
@@ -59,5 +70,5 @@ public class AnalysisServerRunner
 		{
 			servlet.destroy();
 		}
-    }
+	}
 }
